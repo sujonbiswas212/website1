e92ea706b7068ed455695e51d4faff2175fa0a96
sujonbiswas212 <wwwsujonbiswas212@gmail.com>
2025-03-25 19:06:37 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// typing speed js
+
+var typed = new Typed(".typing",{
+     strings:[ "sujon biswas","web developer","and digital marketer"],
+     typeSpeed:80,
+     backSpeed:80,
+})
+
+
+var typed = new Typed(".typing2",{
+     strings:[ "sujon biswas","web developer","and digital marketer"],
+     typeSpeed:80,
+     backSpeed:80,
+})
